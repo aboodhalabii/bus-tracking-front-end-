875977ea366d3c2b8bb159685111942c38793f8f
SulimanMahjna <elriced700@gmail.com>
2026-01-05 14:29:45 +0300

    css

--- BusTracking/src/App.jsx
+++ BusTracking/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './studentLogin';  
 import Dashboard from './dashboard';  
+import './App.css';
+
 
 function App() {
   return (
