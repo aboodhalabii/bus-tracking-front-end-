fdcdb5bd300bdac877b5a7447cf39a63cc8f7df1
SulimanMahjna <elriced700@gmail.com>
2026-01-05 15:01:04 +0300

    all login pages

--- BusTracking/src/App.jsx
+++ BusTracking/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import StudentLogin from './studentLogin';  
-import AdminLogin from './adminLogin';
-import BusDriverLogin from './busdriverLogin'
+import Login from './studentLogin';  
 import Dashboard from './dashboard';  
 import './App.css';
 
@@ -12,9 +10,7 @@ function App() {
     <Router>
       <Routes>
         {/* Define routes */}
-        <Route path="/studentlogin" element={<StudentLogin />} />
-        <Route path="/adminlogin" element={<AdminLogin />} />
-        <Route path="/busdriverlogin" element={<BusDriverLogin />} />
+        <Route path="/studentlogin" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
